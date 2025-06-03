71eaa1d8cde6fa637d326fe5473d16f4604d7342
6teendollars <91662992+6teendollars@users.noreply.github.com>
2025-06-03 14:09:19 +0200

    Deploy Storybook

---
+++ assets/Button.stories-4tGRnsih.js
@@ -0,0 +1,26 @@
+import{B}from"./Button-BDUTzyTw.js";import"./jsx-runtime-DoEZbXM1.js";import"./jsx-runtime-Bw5QeaCk.js";import"./index-Bj7X0qp-.js";const U={title:"Components/Button",component:B,tags:["autodocs"],args:{children:"Test"}},r={args:{intent:"primary",size:"medium"}},e={args:{intent:"secondary",size:"large"}},a={args:{intent:"primary",size:"small"}},s={args:{intent:"primary",size:"large"}},n={args:{intent:"unavailable",size:"large"}};var t,o,i;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    intent: "primary",
+    size: "medium"
+  }
+}`,...(i=(o=r.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};var m,c,p;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    intent: "secondary",
+    size: "large"
+  }
+}`,...(p=(c=e.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var l,d,g;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    intent: "primary",
+    size: "small"
+  }
+}`,...(g=(d=a.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var u,y,z;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    intent: "primary",
+    size: "large"
+  }
+}`,...(z=(y=s.parameters)==null?void 0:y.docs)==null?void 0:z.source}}};var S,b,v;n.parameters={...n.parameters,docs:{...(S=n.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    intent: "unavailable",
+    size: "large"
+  }
+}`,...(v=(b=n.parameters)==null?void 0:b.docs)==null?void 0:v.source}}};const _=["Primary","Secondary","Small","Large","Unavailable"];export{s as Large,r as Primary,e as Secondary,a as Small,n as Unavailable,_ as __namedExportsOrder,U as default};
